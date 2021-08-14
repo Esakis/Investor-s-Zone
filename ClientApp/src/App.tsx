3a343e1e4987729edde9c31bd2f2702a46a6c8ad
Esakis <71234576+Esakis@users.noreply.github.com>
2021-08-15 00:32:01 +0200

    Poprawne logowanie

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,20 +1,47 @@
-import * as React from 'react';
-import { Route } from 'react-router';
-import Layout from './components/Layout';
-import Home from './components/Home';
-import Counter from './components/Counter';
-import FetchData from './components/FetchData';
-
+import React, { useEffect, useState } from "react";
+import NavMenu from "./components/NavMenu";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Register from "./components/Register";
 import './custom.css'
+import { BrowserRouter, Route } from "react-router-dom";
 
 
-export default () => (
-    <Layout>
-       
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data/:startDateIndex?' component={FetchData} />
-  
+
+function App() {
+
+    const [email, setEmail] = useState("");
 
 
-    </Layout>
-);
+    useEffect(() => {
+        (
+            async () => {
+                const response = await fetch('https://localhost:44349/api1/account', {
+                    headers: { 'Content-Type': 'application/json; charset=UTF-8' },
+                    credentials: "include",
+
+                });
+                const content = await response.json();
+                setEmail(content.email);
+
+            }
+        )();
+
+    });
+
+
+    return (
+        <div >
+            <BrowserRouter>
+                <NavMenu email={email} setEmail={setEmail}/>
+
+                <main className="form-signin">
+                    <Route path="/" component={Home}/>
+                    <Route path="/login" component={() => <Login setEmail={setEmail} />} />
+                    <Route path="/register" component={Register} />
+                </main>
+            </BrowserRouter>
+        </div>
+    );
+}
+export default App;
---
+++ UserSeeder.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InvestorZone.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InvestorZone
+{
+    public class UserSeeder
+    {
+        private readonly UserDbContext _dbContext;
+        public UserSeeder(UserDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Seed()
+        {
+            if (_dbContext.Database.CanConnect())
+            {
+                var pendingMigrations = _dbContext.Database.GetPendingMigrations();
+                if (pendingMigrations != null && pendingMigrations.Any())
+                {
+                    _dbContext.Database.Migrate();
+                }
+
+                if (!_dbContext.Roles.Any())
+                {
+                    var roles = GetRoles();
+                    _dbContext.Roles.AddRange(roles);
+                    _dbContext.SaveChanges();
+                }
+                if (!_dbContext.Users.Any())
+                {
+                    var users = GetUsers();
+                    _dbContext.Users.AddRange(users);
+                    _dbContext.SaveChanges();
+                }
+            }
+        }
+
+        private IEnumerable<Role> GetRoles()
+        {
+            var roles = new List<Role>()
+            {
+                new Role()
+                {
+                    Name="User"
+                },
+                 new Role()
+                {
+                    Name="Admin"
+                },
+            };
+            return roles;
+        }
+
+        private IEnumerable<User> GetUsers()
+        {
+            var users = new List<User>()
+            {
+                new User()
+                {
+                    Email="es11@wp.pl",
+                    FirstName="Lukasz",
+                    LastName= "S",
+                    PasswordHash="Haslo1",
+                    RoleId=1,
+                },
+               new User()
+                {
+
+                    Email="ddds11@wp.pl",
+                    FirstName="Domi",
+                    LastName= "B",
+                    PasswordHash="Haslo2",
+                    RoleId=1,
+                },
+               new User()
+                {
+                    Email="ggg11@wp.pl",
+                    FirstName="Grzegorz",
+                    LastName= "P",
+                    PasswordHash="Haslo3",
+                    RoleId=1,
+                },
+               new User()
+                {
+                    Email="aaa1@wp.pl",
+                    FirstName="Artur",
+                    LastName= "A",
+                    PasswordHash="Haslo4",
+                    RoleId=1,
+                },
+            };
+            return users;
+        
+        }
+
+    }
+}
