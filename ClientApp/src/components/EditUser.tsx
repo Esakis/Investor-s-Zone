4d0a318705531926d8a1066c7bead24ff62d986c
Esakis <71234576+Esakis@users.noreply.github.com>
2021-08-17 07:10:44 +0200

    wstepnaedycjauzytkownika

---
+++ ClientApp/src/components/EditUser.tsx
@@ -0,0 +1,114 @@
+﻿import React, { SyntheticEvent, useEffect, useState } from "react";
+import { Link, Redirect } from "react-router-dom";
+
+
+const EditUser = (props: { email: string }) => {
+
+
+     const [dataemail, setDataemail] = useState('');
+     const [datafirstname, setDatafirstname] = useState('');
+     const [datalastname, setDatalastname] = useState('');
+     const [email, setEmail] = useState('');
+     const[firstname,setFirstName]=useState('');
+     const[lastname,setLastName]=useState('');
+    
+     const [redirect, setRedirect] = useState(false);
+ 
+   
+
+
+
+
+
+    useEffect(() => {
+        (
+            async () => {
+                const response = await fetch('https://localhost:44349/api/account/' + props.email, {
+                    method: 'GET',
+                    headers: { 'Content-Type': 'application/json; charset=UTF-8' },
+                    credentials: "include",
+
+                });
+                const content = await response.json();
+                console.log(content);
+                setDataemail(content.email);
+                setDatafirstname(content.firstName);
+                setDatalastname(content.lastName);
+                
+            }
+        )();
+
+    });
+
+
+
+
+    const submit = async (e: SyntheticEvent)=> {
+   
+        e.preventDefault();
+     
+        await fetch('https://localhost:44349/api/account/' + props.email, {
+            method:'PUT',
+            headers:{'Content-Type':'application/json; charset=UTF-8'},
+            body: JSON.stringify({
+                email,
+                firstname,
+                lastname
+            })
+        });
+
+        setRedirect(true);
+    }
+
+    if (redirect) {
+        return <Redirect to="/" />;
+    }
+  
+    return (
+
+
+        <div className="card-body p-5">
+            <h6 >email : {dataemail} First Name: {datafirstname} Last Name: {datalastname}</h6>
+            <li className="nav-item">
+                <Link to={"/api/account/" + props.email} className="nav-link active" >Edytuj dane</Link>
+            </li>
+
+            
+            <h4 className="text-uppercase text-center mb-5">Create an account</h4>
+            
+            <form onSubmit={submit}>
+                <div className="form-floating">
+                    <input type="text" className="form-control" id="floatingInput" placeholder={props.email} required
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                    <label htmlFor="floatingInput">Email</label>
+                </div>
+                <br />
+            <div className="form-floating">
+                    <input type="text" className="form-control" id="floatingInput" placeholder="Name" required
+                        onChange={e => setFirstName(e.target.value)}
+                />
+                <label htmlFor="floatingInput">First Name</label>
+            </div>
+                <br/>
+
+           
+            <div className="form-floating">
+                <input type="text" className="form-control" id="floatingInput" placeholder="Name" required
+                       onChange={e=>setLastName(e.target.value)}/>
+                <label htmlFor="floatingInput">Last Name</label>
+            </div>
+                <br/>
+            
+     
+                        <div className="d-flex justify-content-center">
+                            <button type="submit"
+                                    className="btn btn-success btn-block btn-lg gradient-custom-4 text-body">Edit Data
+                            </button>
+                        </div>
+
+                    </form>
+            </div>
+)
+}
+export default EditUser;
---
+++ Controllers/ForumController.cs
@@ -0,0 +1,92 @@
+﻿using InvestorZone.Entities;
+using InvestorZone.Exceptions;
+using InvestorZone.Models;
+using InvestorZone.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InvestorZone.Controllers
+{
+    [Route("api/forum")]
+    [ApiController]
+    public class ForumController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+        private readonly UserDbContext _context;
+
+        public ForumController(IAccountService accountService, UserDbContext context)
+        {
+            _accountService = accountService;
+            _context = context;
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult<LoginDto> UpdatePost(Forum post)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Not a valid model");
+
+            using (var ctx = new UserDbContext())
+            {
+                var existingPost = ctx.Forum.Where(p => p.Id == post.Id)
+                    .FirstOrDefault<Forum>();
+
+                if (existingPost != null)
+                {
+                    existingPost.Id = post.Id;
+                    existingPost.Post = post.Post;
+                    existingPost.Title = existingPost.Title;
+                    existingPost.UserId = existingPost.UserId;
+                    ctx.SaveChanges();
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+
+            return Ok();
+        }
+
+        [HttpGet("forum")]
+        public ActionResult<IEnumerable<LoginDto>> GetAll()
+        {
+            var forum = _context.Forum.ToList();
+
+            return Ok(forum);
+        }
+
+
+
+        [HttpGet("{id}")]
+
+        public ActionResult<LoginDto> GetById([FromRoute] int id)
+        {
+            var forum = _context
+                .Forum
+                .FirstOrDefault(r => r.Id == id);
+
+            return Ok(forum);
+        }
+
+
+        [HttpDelete("{id}")]
+
+        public void Delete(int id)
+        {
+            var forum = _context
+                .Forum
+                .FirstOrDefault(r => r.Id == id);
+
+            if (forum is null)
+                throw new NotFoundException("User not found");
+
+            _context.Forum.Remove(forum);
+            _context.SaveChanges();
+
+        }
+
+    }
+}
